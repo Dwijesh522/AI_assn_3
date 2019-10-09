c9ed83516ef5a860148de90a5404dac54079fa0a
dwijesh522 <iddwijesh@gmail.com>
2019-10-09 23:09:29 +0530

    provided by profs

---
+++ minisat2.2.0/minisat/core/depend.mk
@@ -0,0 +1,29 @@
+/home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Solver.o /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Solver.or /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Solver.od /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Solver.op: \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Solver.cc \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Sort.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Vec.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/IntTypes.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/XAlloc.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Solver.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Heap.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Alg.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/utils/Options.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/utils/ParseUtils.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/SolverTypes.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Map.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Alloc.h
+/home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Main.o /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Main.or /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Main.od /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Main.op: \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Main.cc \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/utils/System.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/IntTypes.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/utils/ParseUtils.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/utils/Options.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Vec.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/XAlloc.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Dimacs.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/SolverTypes.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Alg.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Map.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Alloc.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/core/Solver.h \
+ /home/dwijesh/Documents/sem5/ai/assns/assn3/AI_assn_3/minisat2.2.0/minisat/mtl/Heap.h
